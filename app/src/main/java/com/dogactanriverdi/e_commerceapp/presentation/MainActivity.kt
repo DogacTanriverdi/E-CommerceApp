5a3fc100a3fcda02ccf8aa268b46f74f19904558
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-04-15 19:06:08 +0300

    Complete the BottomNavBar menu and show it in MainActivity

--- app/src/main/java/com/dogactanriverdi/e_commerceapp/presentation/MainActivity.kt
+++ app/src/main/java/com/dogactanriverdi/e_commerceapp/presentation/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.dogactanriverdi.e_commerceapp.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.dogactanriverdi.e_commerceapp.R
+import com.dogactanriverdi.e_commerceapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        NavigationUI.setupWithNavController(
+            binding.bottomNavView,
+            navHostFragment.navController
+        )
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.signInFragment,
+                R.id.signUpFragment,
+                R.id.cartFragment -> {
+                    binding.bottomNavView.visibility = View.GONE
+                }
+
+                else -> {
+                    binding.bottomNavView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
