887621d18653c1f7cbfb3e82f2ce54d828c46ef1
Dogac Tanriverdi <d.tnrvrdi@proton.me>
2024-04-16 08:28:31 +0300

    Add ViewBindingDelegate file & Complete SignIn and SignUp fragment layouts and navigations

--- app/src/main/java/com/dogactanriverdi/e_commerceapp/presentation/signin/SignInFragment.kt
+++ app/src/main/java/com/dogactanriverdi/e_commerceapp/presentation/signin/SignInFragment.kt
@@ -1,7 +1,31 @@
 package com.dogactanriverdi.e_commerceapp.presentation.signin
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.dogactanriverdi.e_commerceapp.R
+import com.dogactanriverdi.e_commerceapp.common.viewBinding
+import com.dogactanriverdi.e_commerceapp.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment(R.layout.fragment_sign_in) {
+
+    private val binding by viewBinding(FragmentSignInBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            tvDontHaveAnAccount.setOnClickListener {
+                val action = SignInFragmentDirections.actionSignInFragmentToSignUpFragment()
+                findNavController().navigate(action)
+            }
+
+            btnSignIn.setOnClickListener {
+                val action = SignInFragmentDirections.actionSignInFragmentToHomeFragment()
+                findNavController().navigate(action)
+            }
+        }
+    }
 }
--- app/src/main/java/com/dogactanriverdi/e_commerceapp/presentation/signup/SignUpFragment.kt
+++ app/src/main/java/com/dogactanriverdi/e_commerceapp/presentation/signup/SignUpFragment.kt
@@ -1,7 +1,31 @@
 package com.dogactanriverdi.e_commerceapp.presentation.signup
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.dogactanriverdi.e_commerceapp.R
+import com.dogactanriverdi.e_commerceapp.common.viewBinding
+import com.dogactanriverdi.e_commerceapp.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+
+    private val binding by viewBinding(FragmentSignUpBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+
+            tvAlreadyHaveAnAccount.setOnClickListener {
+                val action = SignUpFragmentDirections.actionSignUpFragmentToSignInFragment()
+                findNavController().navigate(action)
+            }
+
+            btnSignUp.setOnClickListener {
+                val action = SignUpFragmentDirections.actionSignUpFragmentToHomeFragment()
+                findNavController().navigate(action)
+            }
+        }
+    }
 }
